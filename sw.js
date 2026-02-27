e68640ce900d3297bd549803492073dac7fda383
PuttGame <haputt22@gmail.com>
2026-02-27 15:54:47 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,36 @@
-const CACHE_NAME = 'budugo-v1';
+const CACHE_NAME = 'budugo-cache-v3';
+
 const urlsToCache = [
-    '/budugo/',
-    '/budugo/index.html',
-    '/budugo/manifest.json'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap',
+  'https://fonts.gstatic.com/s/pressstart2p/v15/5aR4gnAOmFHLn2X3bl5t.woff2'
+  // tambahkan icon jika ingin cache juga: 'icons/icon-192x192.png', dll.
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
